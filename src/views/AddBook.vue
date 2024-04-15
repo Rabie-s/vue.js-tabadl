6a5b4fb51234252c414244804f5adf5db0e06dd5
Rabie-s <asdfw.jsd@gmail.com>
2024-04-15 18:36:31 +0300

    .

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -4,47 +4,47 @@
         <div class="bg-gray-200 p-5 m-14 rounded-lg">
             <h1 class="text-2xl text-center font-bold">طلب او عرض كتاب لا تحتاجة</h1>
 
-            <Form @submit="submit" method="post" :validation-schema="schema">
+            <form @submit.prevent="submit" method="post">
 
                 <div class="flex flex-col">
                     <label class="m-1 text-sm">صورة الغلاف</label>
-                    <Field name="image" @change="onFileChange" type="file" class="outline outline-none
+                    <input ref="inputImage" accept=".png, .jpg, .jpeg" @change="onFileChange" type="file" class="outline outline-none
                      rounded w-full p-1 file:bg-sky-600
                       hover:file:bg-sky-500 file:p-2
                        file:rounded-lg file:text-white
                         file:outline-none file:border-0
                          file:cursor-pointer" />
-                    <ErrorMessage class="text-red-500" name="image" />
+                    <span class="text-red-500" v-for="error in v$.image.$errors">{{ error.$message }}</span>
+
                 </div>
 
                 <div class="flex flex-col">
                     <label class="m-1 text-sm">اسم الكتاب</label>
-                    <Field name="title" v-model="formData.title"
-                        class="bg-white text-sm h-[27px] outline-none rounded-lg px-1" />
-                    <ErrorMessage class="text-red-500" name="title" />
+                    <input v-model="formData.title" class="bg-white text-sm h-[27px] outline-none rounded-lg px-1" />
+                    <span class="text-red-500" v-for="error in v$.title.$errors">{{ error.$message }}</span>
                 </div>
 
                 <div class="flex flex-col">
                     <label class="m-1 text-sm">وصف قصير</label>
-                    <Field name="description" v-model="formData.description"
+                    <input v-model="formData.description"
                         class="bg-white text-sm h-[27px] outline-none rounded-lg px-1" />
-                    <ErrorMessage class="text-red-500" name="description" />
+                    <span class="text-red-500" v-for="error in v$.description.$errors">{{ error.$message }}</span>
                 </div>
 
                 <label class="m-1 text-sm">طلب ولا عرض؟</label>
-                <Field as="select" name="status" v-model="formData.status"
-                    class="w-full bg-white text-sm h-[27px] outline-none rounded-lg px-1">
+                <select v-model="formData.status" class="w-full bg-white text-sm h-[27px] outline-none rounded-lg px-1">
                     <option value="1">معروض</option>
                     <option value="2">مطلوب</option>
-                </Field>
-                <ErrorMessage class="text-red-500" name="status" />
-                
+                </select>
+                <span class="text-red-500" v-for="error in v$.status.$errors">{{ error.$message }}</span>
+
+
                 <div class="mt-5 flex flex-col items-center gap-y-2">
-                    <Button color="blue">نشر</Button>
+                    <Button type="submit" color="blue">نشر</Button>
                 </div>
 
 
-            </Form>
+            </form>
 
         </div>
     </div>
@@ -54,34 +54,41 @@
 
 <script setup>
 import Button from '@/components/Button.vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
 import { ref } from 'vue'
-import { Field, Form, ErrorMessage } from 'vee-validate';
-import * as yup from 'yup';
 import axios from 'axios'
 import { toast } from 'vue3-toastify';
 import { useUserStore } from '@/stores/user.js'
 const user = useUserStore();
 
 
-//form validation
-const schema = yup.object({
-    image: yup.mixed().required('Image is required'),
-    title: yup.string().required(),
-    description: yup.string().required(),
-    status: yup.string().required()
-});
 
+const inputImage = ref(null)
 const formData = ref({
     image: '',
     title: '',
     status: '',
     description: '',
 })
-
 //catch a file inside file input
 function onFileChange(e) {
     formData.value.image = e.target.files[0]
 }
+
+//form rules validation
+const rules = {
+    image: { required },
+    title: { required },
+    status: { required },
+    description: { required }
+}
+
+const v$ = useVuelidate(rules, formData)
+
+
+
+
 
 async function submit() {
     await axios.post('/books', {
@@ -92,22 +99,27 @@ async function submit() {
         user_id: user.userData.id
     }, {
         headers: {
-            'Content-Type':'multipart/form-data'
+            'Content-Type': 'multipart/form-data'
         }
-    }).then((response) => {
-        console.log(response.data)
-        toast.success('تم نشر الاعلان بنجاح',{"theme": "colored"})
-        
+    }).then(async (response) => {
+        const result = await v$.value.$validate()
+        if (result) {
+            console.log(response.data)
+            toast.success('تم نشر الاعلان بنجاح', { "theme": "colored" })
+            clear()
+        }
     }).catch((error) => {
         console.log(error)
     });
 }
 
-function clear(){
+function clear() {
     formData.value.image = ''
     formData.value.title = ''
     formData.value.status = ''
     formData.value.description = ''
+    inputImage.value.value = null
+    v$.value.$reset()
 }
 
 </script>
