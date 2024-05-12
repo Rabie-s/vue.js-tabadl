cddd1e5a670cc3ed510342473512fc25f4fecea9
Rabie-s <asdfw.jsd@gmail.com>
2024-05-12 23:30:07 +0300

    add gurds to routs

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,68 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user.js'
 
+// Define routes
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/LoginView.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/RegisterView.vue')
+  },
+  {
+    path: '/books',
+    name: 'Books',
+    component: () => import('@/views/BooksView.vue')
+  },
+  {
+    path: '/book/:id',
+    name: 'Book',
+    component: () => import('@/views/BookView.vue')
+  },
+  {
+    path: '/add-book',
+    name: 'AddBook',
+    meta: { requiresAuth: true }, // Metafield to indicate authentication requirement
+    component: () => import('@/views/AddBook.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import('@/views/TestView.vue')
+  },
+]
+
+// Create router instance
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('@/views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: () => import('@/views/RegisterView.vue')
-    },
-    {
-      path: '/books',
-      name: 'Books',
-      component: () => import('@/views/BooksView.vue')
-    },
-    {
-      path: '/book/:id',
-      name: 'Book',
-      component: () => import('@/views/BookView.vue')
-    },
-    {
-      path: '/add-book',
-      name: 'AddBook',
-      meta: {
-        requiresAuth: true
-      },
-      component: () => import('@/views/AddBook.vue')
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import('@/views/TestView.vue')
-    },
-
-  ]
+  routes
 })
 
+// Navigation guard to check authentication
+router.beforeEach(async (to, from, next) => {
+  const user = useUserStore()
+
+  // Redirect to login if route requires authentication and user is not authenticated
+  if (to.meta.requiresAuth && !user.isAuth) {
+    next('/login')
+  } 
+  // Redirect to home if user is already authenticated and tries to access login or register pages
+  else if ((to.name === 'Login' || to.name === 'Register') && user.isAuth) {
+    next('/')
+  } 
+  // Otherwise, proceed with navigation
+  else {
+    next()
+  }
+})
 
 export default router
